e257f98d6b3b0fd85237f4f22e727bc6dc7455a4
Krivie_Ruki <morozm87@mail.ru>
2022-04-25 20:55:16 +0300

    rever v1 withaot orig

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -19,6 +19,6 @@ from. import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
-    path ("new/",views.new)
+    path ("reverse/",views.reverse,name="reverse")
 ]
 
